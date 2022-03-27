6a66a9dd2048bce948d9c755f3d55b37b1a3fbe7
pchaberski <chaberski.piotr@gmail.com>
2022-03-27 01:46:44 +0100

    added image resizing

--- data/hm_data.py
+++ data/hm_data.py
@@ -4,6 +4,7 @@
 
 import os
 from torch.utils.data import Dataset
+from torchvision import transforms
 from PIL import Image
 
 
@@ -11,16 +12,27 @@ class HMDataset(Dataset):
     """Class for H&M Images."""
 
     def __init__(
-        self, data_path
+        self,
+        data_path,
+        image_size=[224, 224]
     ):
         super().__init__()
         self.data_path = data_path
+        self.image_size=image_size
         self.image_fnames = self._get_image_fnames(data_path)
 
     def _get_image_fnames(self, data_path):
         jpg_fnames = [file for file in os.listdir(data_path) if file.endswith('.jpg')]
 
         return jpg_fnames
+
+    def _transform(self, image):
+        # Resizing
+        transf_list = [transforms.Resize(self.image_size)]
+
+        transf = transforms.Compose(transf_list)
+
+        return transf(image)
 
     def __len__(self):
         return len(self.image_fnames)
@@ -29,6 +41,8 @@ class HMDataset(Dataset):
         image_fname = self.image_fnames[idx]
         image_fpath = os.path.join(self.data_path, image_fname)
         image = Image.open(image_fpath).convert('RGB')
+
+        image = self._transform(image)
 
         return image
     
