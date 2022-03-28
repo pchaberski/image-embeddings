9b578e76e0d588a25d3fd8493241ae4751e88132
pchaberski <chaberski.piotr@gmail.com>
2022-03-28 23:35:57 +0200

    added normalization coeffs extraction

---
+++ utils/normalization_coeffs.py
@@ -0,0 +1,44 @@
+import sys
+
+sys.path.append('..')
+
+from utils.configuration import load_config
+from data.hm_data import HMDataset
+import torch
+import numpy as np
+import os
+from tqdm import tqdm
+
+cfg = load_config('../config.yml')
+
+data_train = HMDataset(
+    data_path=os.path.join(cfg.get('data_path'), 'train'),
+    image_size=cfg.get('image_size'),
+    normalize=cfg.get('normalize'),
+    normalization_params=cfg.get('normalization_params')
+)
+
+means = {
+    'r': [],
+    'g': [],
+    'b': []
+}
+stds = {
+    'r': [],
+    'g': [],
+    'b': []
+}
+
+print(f'Images path: {data_train.data_path}')
+print('Processing images to extract means and standard deviations...')
+for idx in tqdm(range(len(data_train))):
+    img = data_train[idx].detach().cpu()
+    for i, k in enumerate(means):
+        means[k].append(torch.mean(img[i, :, :]))
+        stds[k].append(torch.std(img[i, :, :]))
+
+print('Results:')
+print(f'  mean: {[round(float(torch.mean(torch.stack(v)).numpy()), 4) for v in means.values()]}')
+print(f'  std: {[round(float(torch.mean(torch.stack(v)).numpy()), 4) for v in stds.values()]}')
+
+
