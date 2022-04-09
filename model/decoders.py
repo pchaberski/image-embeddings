74e4218e7fa324043c4187222676fea4569efecb
pchaberski <chaberski.piotr@gmail.com>
2022-04-09 21:21:25 +0200

    added encoders and decoders with larger linear dim; fixed dims in unflatten

--- model/decoders.py
+++ model/decoders.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 import torch
 
 
-class DecoderBase(nn.Module):
+class DecoderLinearBase(nn.Module):
 
     def __init__(
         self,
@@ -29,6 +29,29 @@ class DecoderBase(nn.Module):
         return x
 
 
+class DecoderLinearLin1024(nn.Module):
+
+    def __init__(
+        self,
+        embedding_size: int = 32,
+    ):
+        super().__init__()
+        self.image_size = [128, 128]
+        self.embedding_size = embedding_size
+
+        self.decoder = nn.Sequential(
+            nn.Linear(self.embedding_size, 1024),
+            nn.ReLU(), 
+            nn.Linear(1024, self.image_size[0]*self.image_size[1]*3)
+        )
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = self.decoder(x)
+
+        return x
+
+
 class DecoderConvBase(nn.Module):
 
     def __init__(
@@ -42,13 +65,49 @@ class DecoderConvBase(nn.Module):
         self.decoder_lin = nn.Sequential(
             nn.Linear(self.embedding_size, 128),
             nn.ReLU(),
-            nn.Linear(128, 802816),
+            nn.Linear(128, 262144),
             nn.ReLU()
         )
 
         self.unflatten = nn.Unflatten(
             dim=1, 
-            unflattened_size=(256, 56, 56)
+            unflattened_size=(256, 32, 32)
+        )
+
+        self.decoder_conv = nn.Sequential(
+            nn.ConvTranspose2d(256, 16, 2, stride = 2, padding = 0),
+            nn.ConvTranspose2d(16, 3, 2, stride = 2)
+        )
+
+    def forward(self, x):
+        x = self.decoder_lin(x)
+        x = self.unflatten(x)
+        x = self.decoder_conv(x)
+        x = torch.sigmoid(x)
+
+        return x
+
+
+class DecoderConvLin2014(nn.Module):
+
+    def __init__(
+        self,
+        embedding_size: int = 32,
+    ):
+        super().__init__()
+        self.image_size = [128, 128]
+        self.embedding_size = embedding_size
+
+        self.decoder_lin = nn.Sequential(
+            nn.Linear(self.embedding_size, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 262144),
+            nn.ReLU()
+        )
+
+        self.unflatten = nn.Unflatten(
+            dim=1, 
+            unflattened_size=(256, 32, 32)
         )
 
         self.decoder_conv = nn.Sequential(
--- model/encoders.py
+++ model/encoders.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class EncoderBase(nn.Module):
+class EncoderLinearBase(nn.Module):
 
     def __init__(
         self,
@@ -19,6 +19,29 @@ class EncoderBase(nn.Module):
             nn.Linear(self.image_size[0]*self.image_size[1]*3, 128), 
             nn.ReLU(), 
             nn.Linear(128, self.embedding_size)
+        )
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = self.encoder(x)
+
+        return x
+
+
+class EncoderLinearLin1024(nn.Module):
+
+    def __init__(
+        self,
+        embedding_size: int = 32,
+    ):
+        super().__init__()
+        self.image_size = [128, 128]
+        self.embedding_size = embedding_size
+
+        self.encoder = nn.Sequential(
+            nn.Linear(self.image_size[0]*self.image_size[1]*3, 1024), 
+            nn.ReLU(), 
+            nn.Linear(1024, self.embedding_size)
         )
 
     def forward(self, x):
@@ -53,9 +76,49 @@ class EncoderConvBase(nn.Module):
         self.flatten = nn.Flatten(start_dim=1)
 
         self.encoder_lin = nn.Sequential(
-            nn.Linear(802816, 128),
+            nn.Linear(262144, 128),
             nn.ReLU(),
             nn.Linear(128, self.embedding_size)
+        )
+
+    def forward(self, x):
+        BS = x.shape[0]
+        x = x.view(BS, 3, self.image_size[0], self.image_size[1])
+        x = self.encoder_conv(x)
+        x = self.flatten(x)
+        x = self.encoder_lin(x)
+
+        return x
+
+
+class EncoderConvLin1024(nn.Module):
+
+    def __init__(
+        self,
+        embedding_size: int = 32,
+    ):
+
+        super().__init__()
+        self.image_size = [128, 128]
+        self.embedding_size = embedding_size
+
+        self.encoder_conv = nn.Sequential(
+            nn.Conv2d(3, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 256, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(256)
+        )
+
+        self.flatten = nn.Flatten(start_dim=1)
+
+        self.encoder_lin = nn.Sequential(
+            nn.Linear(262144, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, self.embedding_size)
         )
 
     def forward(self, x):
