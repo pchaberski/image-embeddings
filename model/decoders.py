5fdd06034dab934fc69a303fc441c7f40e55539a
pchaberski <chaberski.piotr@gmail.com>
2022-04-15 08:47:36 +0200

    added 3 conv layers arch

--- model/decoders.py
+++ model/decoders.py
@@ -158,3 +158,40 @@ class DecoderConvCompr(nn.Module):
         x = torch.sigmoid(x)
 
         return x
+
+
+class DecoderConvCompr3Layer(nn.Module):
+
+    def __init__(
+        self,
+        embedding_size: int = 32,
+    ):
+        super().__init__()
+        self.image_size = [128, 128]
+        self.embedding_size = embedding_size
+
+        self.decoder_lin = nn.Sequential(
+            nn.Linear(self.embedding_size, 128),
+            nn.ReLU(),
+            nn.Linear(128, 8192),
+            nn.ReLU()
+        )
+
+        self.unflatten = nn.Unflatten(
+            dim=1, 
+            unflattened_size=(32, 16, 16)
+        )
+
+        self.decoder_conv = nn.Sequential(
+            nn.ConvTranspose2d(32, 64, 2, stride = 2, padding = 0),
+            nn.ConvTranspose2d(64, 128, 2, stride = 2),
+            nn.ConvTranspose2d(128, 3, 2, stride = 2)
+        )
+
+    def forward(self, x):
+        x = self.decoder_lin(x)
+        x = self.unflatten(x)
+        x = self.decoder_conv(x)
+        x = torch.sigmoid(x)
+
+        return x
